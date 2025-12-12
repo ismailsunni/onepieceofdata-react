59decb11672f19ddcea9b98c83bdd9d92cb6c9cb
Ismail Sunni <imajimatika@gmail.com>
2025-12-12 19:48:34 +0700

    chore: Remove old analytics/ChartCard component - replaced by common/ChartCard with built-in download

--- src/components/analytics/index.ts
+++ src/components/analytics/index.ts
@@ -5,7 +5,6 @@
  * Part of Phase 1: Foundation Components
  */
 
-export { default as ChartCard } from './ChartCard'
 export { default as StatCard } from './StatCard'
 export { default as SectionHeader } from './SectionHeader'
 export { default as FilterButton } from './FilterButton'
